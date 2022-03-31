517164b280c3cba494a69c736c20a559305e2add
lyanchu <yanchu1114@163.com>
2022-03-31 18:17:16 +0800

    3/31

---
+++ 1471.数组中的-k-个最强值.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1471 lang=cpp
+ *
+ * [1471] 数组中的 k 个最强值
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> getStrongest(vector<int>& arr, int k) {
+
+    }
+};
+// @lc code=end
+
